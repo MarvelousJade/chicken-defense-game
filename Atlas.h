30a667cd5e7f3c847a208b6bcc7761c9b475ad75
shaoyu <fanshaoyu9@outlook.com>
2025-06-25 18:31:57 -0400

    Fix naming

--- Atlas.h
+++ Atlas.h
@@ -10,12 +10,12 @@
 #include <stdio.h>
 
 class Atlas {
-    std::vector<SDL_Texture*> textureList; 
+    std::vector<SDL_Texture*> m_textureList; 
 public:
     Atlas() = default;
 
     ~Atlas() {
-        for (SDL_Texture* texture : textureList) {
+        for (SDL_Texture* texture : m_textureList) {
             SDL_DestroyTexture(texture);
         }
     };
@@ -25,26 +25,26 @@ public:
             char pathFile[255];
             std::snprintf(pathFile, sizeof(pathFile), pathTemplate, i + 1);
             SDL_Texture* texture = IMG_LoadTexture(renderer, pathFile);
-            textureList.push_back(texture);
+            m_textureList.push_back(texture);
         }
     }
 
     void clear() {
-        textureList.clear();
+        m_textureList.clear();
     }
 
     int getSize() const {
-        return (int)textureList.size();
+        return (int)m_textureList.size();
     }
 
     SDL_Texture* getTexture(int idx) const {
-        if (idx < 0 || idx >= textureList.size()) return nullptr;
+        if (idx < 0 || idx >= m_textureList.size()) return nullptr;
 
-        return textureList[idx];
+        return m_textureList[idx];
     }
 
     void addTexture(SDL_Texture* texture) {
-        textureList.push_back(texture);
+        m_textureList.push_back(texture);
     }
 };
 
