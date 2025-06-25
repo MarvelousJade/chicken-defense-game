012031be92862c5fe232545fd82b8de2cb5fabc2
shaoyu <fanshaoyu9@outlook.com>
2025-06-25 18:09:04 -0400

    Format Vector2

--- Vector2.h
+++ Vector2.h
@@ -4,65 +4,65 @@
 #include <cmath>
 
 class Vector2 {
-    public:
-        float x = 0;
-        float y = 0;
-        Vector2() = default;
-        ~Vector2() = default;
-        Vector2(float x, float y) 
-            : x(x), y(y) {}
+public:
+    float x = 0;
+    float y = 0;
+    Vector2() = default;
+    ~Vector2() = default;
+    Vector2(float x, float y) 
+        : x(x), y(y) {}
 
-        Vector2 operator+(const Vector2& vec) const {
-            return Vector2(x + vec.x, y + vec.y);
-        }
+    Vector2 operator+(const Vector2& vec) const {
+        return Vector2(x + vec.x, y + vec.y);
+    }
 
-        void operator+=(const Vector2& vec) {
-            x += vec.x;
-            y += vec.y;
-        }
-    
-        Vector2 operator-(const Vector2& vec) const {
-            return Vector2(x - vec.x, y - vec.y);
-        }
+    void operator+=(const Vector2& vec) {
+        x += vec.x;
+        y += vec.y;
+    }
 
-        void operator-=(const Vector2& vec) {
-            x -= vec.x;
-            y -= vec.y;
-        }
+    Vector2 operator-(const Vector2& vec) const {
+        return Vector2(x - vec.x, y - vec.y);
+    }
 
-        float operator*(const Vector2& vec) const {
-            return x * vec.x + y * vec.y;
-        }
+    void operator-=(const Vector2& vec) {
+        x -= vec.x;
+        y -= vec.y;
+    }
 
-        Vector2 operator*(float val) const {
-            return Vector2(x * val, y * val);
-        }
+    float operator*(const Vector2& vec) const {
+        return x * vec.x + y * vec.y;
+    }
 
-        void operator*=(const Vector2& vec) {
-            x *= vec.x;
-            y *= vec.y;
-        }
+    Vector2 operator*(float val) const {
+        return Vector2(x * val, y * val);
+    }
 
-        Vector2 operator/(float val) {
-            return Vector2(x / val, y / val);
-        }
+    void operator*=(const Vector2& vec) {
+        x *= vec.x;
+        y *= vec.y;
+    }
 
-        void operator/=(float val) {
-            x /= val;
-            y /= val;
-        }
+    Vector2 operator/(float val) {
+        return Vector2(x / val, y / val);
+    }
 
-        float length() {
-            return std::sqrt(x * x + y * y);
-        }
+    void operator/=(float val) {
+        x /= val;
+        y /= val;
+    }
 
-        Vector2 normalize() {
-            float len = length();
+    float length() {
+        return std::sqrt(x * x + y * y);
+    }
 
-            if (len ==0) return Vector2(0, 0);
-            
-            return Vector2(x / len, y / len);
-        }
+    Vector2 normalize() {
+        float len = length();
+
+        if (len ==0) return Vector2(0, 0);
+        
+        return Vector2(x / len, y / len);
+    }
 };
 
 #endif
